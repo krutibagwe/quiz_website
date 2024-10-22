7793a14aaad5c11f637b631d772b43b97e1c94bd
Kruti <21kruti@gmail.com>
2024-10-22 21:03:57 +0530

    modified:   src/components/ChooseRole.js

--- src/components/ChooseRole.js
+++ src/components/ChooseRole.js
@@ -18,7 +18,8 @@ const ChooseRole = () => {
             <div className="container h-100">
                 <div className="row h-100 align-items-center justify-content-center">
                     <div className="col-lg-6">
-                        <h1 className='fw-bold mb-4'>Choose Your Role</h1>
+                        <h1 className='fw-bold mb-4'>Welcome to Internet Programming Quiz</h1>
+                        <h2 className='fw-bold mb-4'>Choose Your Role</h2>
                         <div className="d-grid gap-3">
                             <button className="btn btn-light fw-bold w-100" onClick={goToAdminLogin}>
                                 Login as Admin
