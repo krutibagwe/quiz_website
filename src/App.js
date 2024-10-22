1158de88f3d844e457a7fec6608f98fcccd9424d
Kruti <21kruti@gmail.com>
2024-10-22 21:04:51 +0530

    modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,4 @@
-// // src/App.js
-// import React, { useState } from 'react';
-// import Start from './components/Start';
-// import Quiz from './components/Quiz';
-// import Result from './components/Result';
-// import UploadQue from './components/UploadQue'; // Import UploadQue component
-// import { DataProvider } from './context/dataContext';
-
-// function App() {
-//   const [showUploadQue, setShowUploadQue] = useState(false); // State to toggle UploadQue page
-
-//   const toggleUploadPage = () => {
-//     setShowUploadQue(!showUploadQue); // Toggle between quiz and upload form
-//   };
-
-//   return (
-//     <DataProvider>
-//       <div className="App-header">
-//         {/* Button to toggle between Quiz and Upload Question */}
-//         <button className="btn btn-light m-3" onClick={toggleUploadPage}>
-//           {showUploadQue ? "Go to Quiz" : "Upload Questions"}
-//         </button>
-
-//         {/* Show UploadQue component or the Quiz components */}
-//         {showUploadQue ? (
-//           <UploadQue /> // Render UploadQue if toggled on
-//         ) : (
-//           <>
-//             {/* Welcome Page */}
-//             <Start />
-
-//             {/* Quiz Page */}
-//             <Quiz />
-
-//             {/* Result Page */}
-//             <Result />
-//           </>
-//         )}
-//       </div>
-//     </DataProvider>
-//   );
-// }
-
-// export default App;
-
 // src/App.js
-// src/App.js
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ChooseRole from './components/ChooseRole';
@@ -57,7 +9,7 @@ import Start from './components/Start';
 import Quiz from './components/Quiz';
 import Result from './components/Result';
 import UploadQue from './components/UploadQue';
-import { DataProvider } from './context/dataContext'; // Wrap everything in DataProvider
+import { DataProvider } from './context/dataContext'; 
 
 function App() {
     return (
--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import { DataProvider } from './context/dataContext'; // Import the DataProvider
+import { DataProvider } from './context/dataContext'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DataProvider> {/* Wrap App with DataProvider */}
+    <DataProvider> 
       <App />
     </DataProvider>
   </React.StrictMode>
